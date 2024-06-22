3fe27da0493b11d1d4fd47a0242793efd1446b34
Gustavo Santos <gefgu@hotmail.com>
2024-06-22 15:05:30 -0300

    feat: make it a package

---
+++ src/h3cities/__init__.py
@@ -0,0 +1,1 @@
+from .main import get_city_hexagons, get_city_hexagons_geo_df
--- src/h3cities/main.py
+++ src/h3cities/main.py
@@ -1,10 +1,7 @@
 import osmnx as ox
 from h3 import h3
-import contextily as cx
-import matplotlib.pyplot as plt
 import geopandas as gdf
 from shapely.geometry import Polygon, MultiPolygon
-import streamlit as st
 
 
 def swap_lat_lon(coords):
@@ -38,31 +35,3 @@ def get_city_hexagons_geo_df(city_name: str, resolution: int):
     hexagons = get_city_hexagons(city_name, resolution)
     geo_df = gdf.GeoDataFrame(hexagons, crs="EPSG:4326")
     return geo_df
-
-
-def plot_city_hexagons(city_name: str, city: gdf.GeoDataFrame):
-    f, ax = plt.subplots(1, 1, dpi=300)
-
-    city.plot(ax=ax, alpha=0.4, edgecolor="black")
-    cx.add_basemap(ax, crs=city.crs, source=cx.providers.CartoDB.Positron)
-    plt.title(city_name)
-    ax.set_axis_off()
-    return f
-
-
-st.set_page_config(page_title="H3-Cities")
-st.title("H3-Cities First Demo!")
-
-city_name = st.text_input("City Name:", "Paris, France")
-resolution = st.slider("Resolution:", 5, 12, 8)
-
-
-city = get_city_hexagons_geo_df(city_name, resolution)
-fig = plot_city_hexagons(city_name, city)
-
-save_path = f"{city_name.replace(" ", "_").replace(",", "_")}_resolution_{resolution}.geojson"
-
-city.to_file(save_path, driver="GeoJSON")
-st.pyplot(fig)
-with open(save_path, "rb") as f:
-    st.download_button("Export GeoJSON (.geojson)", f, file_name=save_path)
